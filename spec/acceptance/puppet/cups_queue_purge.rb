29d7bc69bcb3c1cc3e017a27915a17bc9038f63c
Thomas Equeter <tequeter@users.noreply.github.com>
2019-02-07 10:05:08 +0100

    silence queue enumeration "normal" errors
    Connecting to Cups may fail because it is not running yet. As Puppet
    tries to enumerate instances before applying the catalog, no amount of
    resource ordering will allow to start the service before we try to
    connect to it.
    
    Furthermore, the desired state might be to stop the service or to not
    manage it at all.
    
    Therefore, purge_unmanaged_queues => true may fail for reasons we can't
    fix, and scare the users with a big red error message (or worse, lead to
    ignore-the-errors mindset).
    
    Thus, silence the error while enumerating Cups queues.

---
+++ spec/acceptance/puppet/cups_queue_purge.rb
@@ -0,0 +1,32 @@
+# frozen_string_literal: true
+
+require 'spec_helper_acceptance'
+
+RSpec.describe 'Custom type `cups_queue`' do
+  context 'when the Cups service is not running' do
+    before(:all) do
+      apply_manifest('class { "cups": service_ensure => "stopped" }', catch_failures: true)
+    end
+
+    it 'does not produce errors' do
+      apply_manifest('class { "cups": purge_unmanaged_queues => true }', catch_failures: true) do
+        assert_not_match(/ipptool: Unable to connect to/, stderr)
+      end
+    end
+  end
+
+  context 'when the Cups service is running' do
+    before(:all) do
+      ensure_cups_is_running
+      purge_all_queues
+      add_printers('Office')
+    end
+
+    let(:manifest) { 'class { "cups": purge_unmanaged_queues => true }' }
+
+    it 'purges unmanaged queues' do
+      apply_manifest(manifest, expect_changes: true)
+      shell('lpstat -p Office', acceptable_exit_codes: [1])
+    end
+  end
+end
